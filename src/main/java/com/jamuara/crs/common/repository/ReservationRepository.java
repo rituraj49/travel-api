5a4ff024e9c1f96938563d09d15a33cea0ebe525
Dhananjay <mr.dhananjaykr2003@gmail.com>
2025-11-25 16:04:09 +0530

    paging

--- src/main/java/com/jamuara/crs/common/repository/ReservationRepository.java
+++ src/main/java/com/jamuara/crs/common/repository/ReservationRepository.java
@@ -2,6 +2,8 @@ package com.jamuara.crs.common.repository;
 
 import com.jamuara.crs.model.Reservation;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -16,5 +18,8 @@ public interface ReservationRepository extends JpaRepository<Reservation,Long> {
     Optional<Reservation> findReservationByBookingId(String id);
 
     Optional<Reservation> findReservationByPnr(String pnr);
+
+    Page<Reservation> findReservationByBookingStatus(Reservation.BookingStatus status, Pageable pageable);
+
 }
 
--- src/main/java/com/jamuara/crs/flight/controller/TboAdminController.java
+++ src/main/java/com/jamuara/crs/flight/controller/TboAdminController.java
@@ -1,37 +1,58 @@
 package com.jamuara.crs.flight.controller;
 
+import com.jamuara.crs.common.service.ReservationService;
 import com.jamuara.crs.flight.dto.tbo.ReservationStatusUpdateDto;
 import com.jamuara.crs.flight.service.TboFlightService;
 import com.jamuara.crs.model.Reservation;
 import jakarta.ws.rs.QueryParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/tbo/admin")
 public class TboAdminController {
     @Autowired
     private TboFlightService tboFlightService;
+    @Autowired
+    private ReservationService reservationService;
 
     @GetMapping("/bookings")
-    public ResponseEntity<?> fetchAllBookings(@QueryParam("status") String status) {
-        List<Reservation> reservations = new ArrayList<>();
+    public ResponseEntity<?> fetchAllBookings(
+            @RequestParam(value = "status", required = false) String status,
+            @RequestParam(value = "page", defaultValue = "0" , required = false) int page,
+            @RequestParam(value = "size", defaultValue = "5", required = false) int size
+    ) {
+        Page<Reservation> reservationsPage;
 
-        if(status != null) {
-            Reservation.BookingStatus statusEnum = null;
-            statusEnum = Reservation.BookingStatus.values()[Integer.parseInt(status) - 1];
-            reservations = tboFlightService.getAllBookings(statusEnum);
+        if (status != null) {
+            Reservation.BookingStatus statusEnum =
+                    Reservation.BookingStatus.values()[Integer.parseInt(status) - 1];
+
+            reservationsPage = reservationService.findReservationsByStatus(statusEnum, page, size);
         } else {
-            reservations = tboFlightService.getAllBookings();
+            reservationsPage = reservationService.findAllReservations(page, size);
         }
 
-        return ResponseEntity.ok().body(reservations);
+        // Build response map
+        Map<String, Object> response = new HashMap<>();
+        response.put("content", reservationsPage.getContent());
+        response.put("pageNumber", reservationsPage.getNumber());
+        response.put("pageSize", reservationsPage.getSize());
+        response.put("totalElements", reservationsPage.getTotalElements());
+        response.put("totalPages", reservationsPage.getTotalPages());
+        response.put("isLastPage", reservationsPage.isLast());
+
+        return ResponseEntity.ok(response);
     }
+
 
     @PostMapping("/bookings/update-status")
     public ResponseEntity<?> updateReservationStatus(@RequestBody ReservationStatusUpdateDto dto) {
